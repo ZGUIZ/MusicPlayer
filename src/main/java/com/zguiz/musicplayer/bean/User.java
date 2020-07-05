f02749eef68c6951eae823165837e67ae08d671e
ZGUIZ <rftoona@outlook.com>
2020-07-05 23:22:11 +0800

    Initial commit

---
+++ src/main/java/com/zguiz/musicplayer/bean/User.java
@@ -0,0 +1,41 @@
+package com.zguiz.musicplayer.bean;
+
+
+public class User {
+    private String userName;
+    private String passWord;
+    private String nickName;
+    private String icon;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
